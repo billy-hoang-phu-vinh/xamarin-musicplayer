fc9774a5600d39029d23790e7fdcc5cd21549edd
phu vinh <billyhoang11.ca@gmail.com>
2021-02-24 14:25:51 -0500

    cover

--- W3Projects/Model/Music.cs
+++ W3Projects/Model/Music.cs
@@ -9,7 +9,7 @@ namespace W3Projects.Model
         public string Title { get; set; }
         public string Artist { get; set; }
         public string Url { get; set; }
-        public string CoverImage { get; set; } = "https://usercontent2.hubstatic.com/14548043_f1024.jpg";
+        public string CoverImage { get; set; } = "https://github.com/billy-hoang-phu-vinh/xamarin-musicplayer/blob/main/sourcefiles/Music_player_logo.png";
         public bool IsRecent { get; set; }
     }
 }
